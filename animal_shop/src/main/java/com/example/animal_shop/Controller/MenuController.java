2e5f955eb77486f99e6118bb45e94b0ec66ae99a
SEとま <toma.gj.0326@gmail.com>
2021-08-29 20:37:29 +0900

    htmlテンプレートとメニューコントローラーの追加

--- animal_shop/src/main/java/com/example/animal_shop/Controller/MenuController.java
+++ animal_shop/src/main/java/com/example/animal_shop/Controller/MenuController.java
@@ -1,14 +1,18 @@
-package com.example.animal_shop;
+package com.example.animal_shop.Controller;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 @Controller
 @EnableAutoConfiguration
-public class Helloworld {
-    @RequestMapping("/hello")
+public class MenuController {
+    @RequestMapping("/")
     @ResponseBody
-    private String hello(){
-        return "Hello World!";
+    private ModelAndView index(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("menu.html");
+        return mav;
     }
 }
